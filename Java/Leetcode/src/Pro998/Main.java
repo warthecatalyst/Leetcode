9a52064e8a2f4a3c077a750fd4d719733f57cabb
wartheCatalyst <18317011442@163.com>
2022-08-30 16:44:29 +0800

    add Pro998

---
+++ Java/Leetcode/src/Pro998/Main.java
@@ -0,0 +1,42 @@
+package Pro998;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode parent = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (val > cur.val) {
+                if (parent == null) {
+                    return new TreeNode(val, root, null);
+                }
+                TreeNode node = new TreeNode(val, cur, null);
+                parent.right = node;
+                return root;
+            } else {
+                parent = cur;
+                cur = cur.right;
+            }
+        }
+        parent.right = new TreeNode(val);
+        return root;
+    }
+}
